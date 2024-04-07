7a00db50e9abe17c0511d40ce38c7bbe7488704d
jureuk7 <jureuk7@protonmail.com>
2024-04-07 19:00:32 +0900

    Feat: Checkbox component

---
+++ src/app/_components/checkbox/Checkbox.tsx
@@ -0,0 +1,18 @@
+import Check from '@/app/_assets/icons/check.svg';
+import styles from './Checkbox.module.scss';
+
+interface CheckboxProps {
+  checked: boolean;
+  onChange: () => void;
+}
+
+export default function Checkbox({ onChange, checked }: CheckboxProps) {
+  return (
+    <div
+      className={[styles.container, checked ? styles.active : ''].join(' ')}
+      onClick={onChange}
+    >
+      <Check fill={'currentColor'} />
+    </div>
+  );
+}
