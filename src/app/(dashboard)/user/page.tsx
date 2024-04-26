d1204be9a40dac57040ea066d97568d712915414
jureuk7 <jureuk7@protonmail.com>
2024-04-26 15:36:30 +0900

    Feat: 유저 관리 페이지

--- src/app/(dashboard)/user/page.tsx
+++ src/app/(dashboard)/user/page.tsx
@@ -1,5 +1,103 @@
-import React from 'react';
+import Sidebar from '../../_components/sidebar/Sidebar';
+import styles from './page.module.scss';
+import { Typography } from '../../_components/typography';
+import InputBox from '../../_components/input/InputBox';
+import Checkbox from '../../_components/checkbox/Checkbox';
+import { cookies } from 'next/headers';
+import { User } from '@/app/types/user';
 
-export default function page() {
-  return <div>page</div>;
+async function getUsers() {
+  'use server';
+  try {
+    console.log(cookies().get('Access')?.value);
+    const response = await fetch('http://localhost:3000/user', {
+      credentials: 'include',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + cookies().get('Access')?.value,
+      },
+    });
+    const users = await response.json();
+    return users;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Page() {
+  const users = await getUsers();
+  return (
+    <div className={styles.section}>
+      <div className={styles.sectionTop}>
+        <Typography.TitleLarge color={90}>유저 관리 관리</Typography.TitleLarge>
+        <Typography.Label color={60}>
+          공지를 작성하거나 관리할 수 있습니다
+        </Typography.Label>
+      </div>
+      <form>
+        <InputBox type="text" placeholder="제목을 입력하세요" />
+      </form>
+      <table className={styles.table}>
+        <thead className={styles['table-head']}>
+          <tr>
+            <th className={styles['table-checkbox']}>
+              <Checkbox checked={true} />
+            </th>
+
+            <th className={styles['table-row']}>
+              <Typography.Body bold color={60}>
+                이메일
+              </Typography.Body>
+            </th>
+            <th className={styles['table-row']}>
+              <Typography.Body bold color={60}>
+                이름
+              </Typography.Body>
+            </th>
+            <th className={styles['table-row']}>
+              <Typography.Body bold color={60}>
+                학번
+              </Typography.Body>
+            </th>
+            <th className={styles['table-row']}>
+              <Typography.Body bold color={60}>
+                학과
+              </Typography.Body>
+            </th>
+            <th className={styles['table-row']}>
+              <Typography.Body bold color={60}>
+                가입 일시
+              </Typography.Body>
+            </th>
+            <th className={styles['table-row']}></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: User) => (
+            <tr key={user.id}>
+              <td className={styles['table-checkbox']}>
+                <Checkbox checked={true} />
+              </td>
+              <td className={styles['table-row']}>
+                <Typography.Body color={80}>{user.email}</Typography.Body>
+              </td>
+              <td className={styles['table-row']}>
+                <Typography.Body color={80}>{user.username}</Typography.Body>
+              </td>
+              <td className={styles['table-row']}>
+                <Typography.Body color={80}>{user.class}</Typography.Body>
+              </td>
+              <td className={styles['table-row']}>
+                <Typography.Body color={80}>{user.department}</Typography.Body>
+              </td>
+              <td className={styles['table-row']}>
+                <Typography.Body color={80}>{user.createdAt}</Typography.Body>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
