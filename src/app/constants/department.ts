ecce9148d3decbd157bc50dcfa3254d6c70093a6
jureuk7 <jureuk7@protonmail.com>
2024-04-26 15:37:14 +0900

    Feat: 동아리 분류, 페이지 섹션, 동아리, 사용자 타입 spec

---
+++ src/app/constants/department.ts
@@ -0,0 +1,10 @@
+export const department: {
+  [key: string]: string;
+} = {
+  소프트웨어과: 'software',
+  정보보호과: 'security',
+  콘텐츠디자인과: 'design',
+  IT경영과: 'business',
+  자율동아리: 'autonomous',
+  일반동아리: 'general',
+};
