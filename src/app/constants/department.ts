a8fcd32864101b0d19152033d2253f2d1890970b
jureuk7 <jureuk7@protonmail.com>
2024-04-26 16:28:04 +0900

    Feat: department constants to korean, assets

--- src/app/constants/department.ts
+++ src/app/constants/department.ts
@@ -8,3 +8,14 @@ export const department: {
   자율동아리: 'autonomous',
   일반동아리: 'general',
 };
+
+export const departmentToKorean: {
+  [key: string]: string;
+} = {
+  software: '소프트웨어과',
+  security: '정보보호과',
+  design: '콘텐츠디자인과',
+  business: 'IT경영과',
+  autonomous: '자율동아리',
+  general: '일반동아리',
+};
