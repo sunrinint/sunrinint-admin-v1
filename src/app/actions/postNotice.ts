e73c6e5a9e1f616ab0204445021c0dd12c954ff4
jureuk7 <jureuk7@protonmail.com>
2024-04-26 15:36:43 +0900

    Feat: 공지 추가 action

---
+++ src/app/actions/postNotice.ts
@@ -0,0 +1,26 @@
+'use server';
+import { revalidateTag } from 'next/cache';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export async function postNotice(formData: FormData) {
+  try {
+    const response = await fetch('http://localhost:3000/notice', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'test',
+        content: formData.get('content'),
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + cookies().get('Access')?.value,
+      },
+    });
+    if (response.status === 201) {
+      revalidateTag('notice');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+  await redirect('/notice?create=false');
+}
